ee4b3c3fbba14d3fb62dccbcef0e61163f9d549b
Tim Staley <tim.staley@astro.ox.ac.uk>
2014-06-11 14:04:10 +0100

    Import debug/release build macros, detailed compiler flags.

--- CMake/CoelaBuildMacros.cmake
+++ CMake/CoelaBuildMacros.cmake
@@ -1,5 +1,45 @@
-#-----------------------------------------------------------------------------------
-# coela_add_library():
+
+#------------------------------------------------------------------------------
+# coela_print_compiler_flags()
+# -Print a concatenated list of all compiler flags 
+#  (can check this for yourself, try `make VERBOSE=1`, but this makes it more 
+#  obvious).
+macro(coela_print_compiler_flags)
+  if (CMAKE_BUILD_TYPE STREQUAL "debug")
+  message(STATUS "CXX_FLAGS=${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+  elseif (CMAKE_BUILD_TYPE STREQUAL "release")
+  message(STATUS "CXX_FLAGS=${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE}")
+  endif()
+endmacro(coela_print_compiler_flags)
+
+#------------------------------------------------------------------------------
+# coela_determine_build_type()
+# -sets CMAKE_BUILD_TYPE depending upon the current build directory location,
+#  relative to the top-level CMakeLists file.
+#  i.e. are we in TOPDIR/build/debug or TOPDIR/build/release?
+#  NB Only runs if CMAKE_BUILD_TYPE is not already set.
+#  If the build-dir path does not match the presets, default to debug-build.
+macro(coela_determine_build_type)
+  if (NOT CMAKE_BUILD_TYPE )
+    if ( "${CMAKE_CURRENT_SOURCE_DIR}/build/debug" STREQUAL "${PROJECT_BINARY_DIR}" )
+      set( CMAKE_BUILD_TYPE debug)
+      message(STATUS "Building in debug mode (debug build subfolder used)")
+    elseif ( "${CMAKE_CURRENT_SOURCE_DIR}/build/release" STREQUAL "${PROJECT_BINARY_DIR}" )
+      set( CMAKE_BUILD_TYPE release)
+      message(STATUS "Building in release mode (release build subfolder used)")
+    else()
+      set( CMAKE_BUILD_TYPE debug)
+      message(STATUS "Building in debug mode (default)")
+    endif()
+
+  else()
+    message(STATUS "Building in ${CMAKE_BUILD_TYPE} mode (user specified)")
+  endif(NOT CMAKE_BUILD_TYPE )
+endmacro(coela_determine_build_type)
+
+
+#------------------------------------------------------------------------------
+# coela_add_library(exec_name ${lib_sources}):
 # -Adds library target
 # -Installs shared/static build to ${LIB_FOLDER}, 
 # -Adds '_debug' suffix if CMAKE_BUILD_TYPE is debug.
@@ -14,10 +54,10 @@ macro(coela_add_library _name)
 #  set_target_properties(${_name} PROPERTIES PREFIX ${LIB_PREFIX})
   set_target_properties(${_name} PROPERTIES OUTPUT_NAME_DEBUG ${_name}_debug)
 endmacro(coela_add_library)
-#-----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------
-# coela_add_executable():
+#------------------------------------------------------------------------------
+# coela_add_executable(exec_name ${exec_sources}):
 # -Adds executable target
 # -Installs target to ${BIN_FOLDER}, 
 # -Adds '_debug' suffix if CMAKE_BUILD_TYPE is debug.
@@ -29,7 +69,7 @@ macro(coela_add_executable _name)
 endmacro(coela_add_executable)
 
 
-#-----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # General unit-testing related macros
 # (Makes use of UnitTest++ framework)
 
@@ -42,9 +82,10 @@ add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} )
 #Place unittests in a separate binary folder to general usage programs:
 set(TEST_BIN_FOLDER ${CMAKE_BINARY_DIR}/unittests )
 
-# coela_add_test_exec()
+# coela_add_test_exec(exec_name ${exec_sources})
 # -Add test executable
 # -Change binary dir from default
+# -Add _debug suffix if building in debug mode.
 # -Add to 'make check' dependency list
 # -Run unit-test in project-specific working directory,
 #  to separate any output files from other projects.
@@ -53,6 +94,7 @@ set(TEST_BIN_FOLDER ${CMAKE_BINARY_DIR}/unittests )
 macro(coela_add_test_exec _name)
   add_executable(${_name} ${ARGN})
   install(TARGETS ${_name} DESTINATION ${TEST_BIN_FOLDER})
+  set_target_properties(${_name} PROPERTIES OUTPUT_NAME_DEBUG ${_name}_debug)
   add_dependencies(check ${_name})    
 
   file(MAKE_DIRECTORY ${TEST_BIN_FOLDER}/${PROJECT_NAME} )
@@ -62,4 +104,4 @@ macro(coela_add_test_exec _name)
           )
 endmacro(coela_add_test_exec)
 
-#-----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
--- build_scripts/make_check_and_install.sh
+++ build_scripts/make_check_and_install.sh
@@ -2,9 +2,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source ${SCRIPT_DIR}/setup_env_vars.sh
 
-mkdir -p ${SCRIPT_DIR}/build
-cd ${SCRIPT_DIR}/build
-cmake -DCMAKE_INSTALL_PREFIX=${COELA_INSTALL} ${SCRIPT_DIR}
-make  check -j$NCPU
-make install -j$NCPU
 
+for BUILD_TYPE in debug release; do
+    mkdir -p ${SCRIPT_DIR}/build/${BUILD_TYPE}
+    cd ${SCRIPT_DIR}/build/${BUILD_TYPE}
+    cmake -DCMAKE_INSTALL_PREFIX=${COELA_INSTALL} ${SCRIPT_DIR}
+    make  check -j$NCPU
+    make install -j$NCPU
+done
